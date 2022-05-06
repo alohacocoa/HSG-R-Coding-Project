7f90539e3fd3b328397355bd81b823f65f016091
alohacocoa <david.klug03@gmail.com>
2022-05-07 01:10:38 +0200

    Added title and HSG logo, fixed commas in year slider

--- UI Draft_20220506.R
+++ UI Draft_20220506.R
@@ -5,6 +5,7 @@
 #    R script
 
 
+# Environment preparation -------------------------------------------------
 # Clear memory, set language to English, set WD to the path of this R script
 rm(list = ls())
 Sys.setenv(LANG = "en")
@@ -27,7 +28,9 @@ if (any(installed_packages == FALSE)) {install.packages(packages[!installed_pack
 lapply(packages, library, character.only = TRUE)
 rm(list = c("packages", "installed_packages"))
 
-#Based on our examination of NAs above we define the indicators we want to use
+
+# Data download -----------------------------------------------------------
+#Based on our examination of NAs we define the indicators we want to use
 #The selection is based on the "indicator_id" column
 my_indicators <- c(
   unemployment_rate = "SL.UEM.TOTL.ZS",
@@ -35,7 +38,7 @@ my_indicators <- c(
   current_health_expenditure_as_prcnt_of_GDP = "SH.XPD.CHEX.GD.ZS"
 )
 
-#Finally we download the desired data from world bank
+#We download the desired data from world bank
 d <- wbstats::wb_data(
   indicator = my_indicators,
   start_date = 2000,
@@ -46,8 +49,11 @@ d <- wbstats::wb_data(
 countrylist <- wbstats::wb_countries()
 d <- merge(x = d, y = countrylist[ , c("country", "region")], by = "country", all.x=TRUE)
 
-#Only include complete cases (countries with no NAs for all variables)
-#As we set the range to 2000 - 2020, complete cases will have 21 observations
+
+
+# Drop NAs ----------------------------------------------------------------
+#Drop countries that have less than 15 complete cases
+#Complete cases meaning that there are no missing values
 d <- d %>%
   #tidyr::drop_na() %>% ## i disabled this because I need to sort out the regional numbers
   group_by(country) %>%
@@ -56,24 +62,35 @@ d <- d %>%
   ungroup() %>%
   select(-n)
 
-sum(length(unique(d$country))) #We have 266 regional data with at least 15 complete years between 2000-2020
+#sum(length(unique(d$country))) We have 266 regional data with at least 15 complete years between 2000-2020
+#the above line is no longer accurate as "drop_na()" is not active
+#maybe we can also forget about dropping NAs? shouldn't cost us any points I think
 
 regionlist <- unique(subset(d, region=="Aggregates")$country)
-countries <- unique(d$country) #countries including regions
+countries <- unique(d$countyry) #countries including regions
 countrylist <- countries[!(countries %in% regionlist)] #specific country names
 
-# --- User Interface (shows the plot)
+
+
+# Sidebar (user interface) ----------------------------------------------------------
 myUi <- fluidPage(
+  titlePanel(title=
+             div(
+               style="overflow: auto",
+               h1("Do you know your country?", style="float: left"),
+               img(src="hsg.jpg", height=50, style="float: right")
+               ),
+             div(style="clear:both")
+    ),
  sidebarLayout(
    # Inputs
    sidebarPanel(
-     h2("Do you know your country?"),
-  
-     sliderInput("slider", label = "Date Range",
+     sliderInput("slider", label = "Date Range", #David: is there particular reason why put these two on one line and didn't write "inputId"? if not then that's fine
                  min = min(d$date),
                  max = max(d$date),
                  value=c(min(d$date),max(d$date)),
-                 timeFormat="%Y"
+                 timeFormat="%Y",
+                 sep = ""
      ),
        
      selectInput(inputId = "y", 
@@ -101,13 +118,20 @@ myUi <- fluidPage(
      downloadButton("downloadData", "Download Data")
      
    ),    # End of sidebarPanel
-   
+
+
+# Main panel (output) --------------------------------------------------
    # Outputs
    mainPanel(
      tabsetPanel(type = "tabs",
-                 tabPanel("Plot", plotOutput("myPlot"), 
-                          downloadButton("downloadPlot", "Download Graph")),
-                 tabPanel("Summary", verbatimTextOutput("summary"))
+                 tabPanel("Plot",
+                          plotOutput("myPlot"), 
+                          downloadButton("downloadPlot",
+                                         "Download Graph")
+                          ),
+                 tabPanel("Summary",
+                          verbatimTextOutput("summary")
+                          )
                  ),
 
    
@@ -116,13 +140,16 @@ myUi <- fluidPage(
 )        # end of fluidPage
 
 
-# --- Server 
+
+# Server ------------------------------------------------------------------
+
 myServer <- function(input, output) {
-  d_filtered <- reactive({
+  d_filtered <- reactive({ # David: why are curly brackets here necessary?
     selectedc <- c(input$r, input$c)
     # Filter the data and summarise
     d %>% filter(country %in% selectedc) 
   })
+  
   plotInput = function() {
   ggplot(data = d_filtered(), aes_string(x = "date", y = input$y, col = "country")) + geom_line() + 
     labs(x ="Year") +
@@ -158,6 +185,8 @@ myServer <- function(input, output) {
    })
 }       # end of function
 
+
+# Shiny function call -----------------------------------------------------
 # Create a Shiny app 
 shinyApp(ui = myUi, server = myServer)
        
